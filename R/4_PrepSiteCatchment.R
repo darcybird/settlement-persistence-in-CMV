ecee8ad6efb87b41f76866521db26cb907c52a27
Darcy Bird <darcyabird@gmail.com>
2023-09-22 11:16:19 -0700

    cleaned script 4, script 7 finalized

--- R/4_PrepSiteCatchment.R
+++ R/4_PrepSiteCatchment.R
@@ -8,53 +8,55 @@ handplant <- terra::rast("data/data-derived/soils/vepiin_hand_planting_200.tif")
 empty_rast <- handplant
 values(empty_rast) <- 1
 
-# make reese catchments areas ----
+# make cells catchments areas ----
 reese <- readr::read_csv(
   here::here(
-    "data/data-raw/Reese2021/4-occupation-by-household-public.csv"
+    "data/data-raw/Kelsey_data/4-occupation-by-household.csv"
   )) %>% 
   tidyr::pivot_longer(cols = c(X450:X1300), names_to = "year", values_to = "nHouse") %>% 
   dplyr::mutate(year = as.numeric (stringr::str_remove_all(year, "X"))) %>% 
-  dplyr::filter(year %in% c(600:1300)) %>% 
-  dplyr::filter(nHouse > 0)
-
+  dplyr::filter(year %in% c(600:1300),
+                nHouse > 0)  
 
 # Join all arch sites on the same cell and consider them one settlement.
-reese_cells <- 
-  reese %>% 
+cells_sites <- 
+  reese %>%
   # join with randomly generated cell locations. Note that sites on the same cell should match the same dummy cell. 
   # The distances between the cells will not be maintained, however
   dplyr::left_join(readr::read_csv(here::here("data/data-raw/Reese2021/dummyCells.csv")),
                    by = c("SITE_ID" = "SITE_ID", "SITE_NO" = "SITE_NO")) %>% 
   #rename so the rest of the script works 
-  dplyr::rename(cell = dummyCell) 
-# the below is the script run, but is dependent on locational information, which we cannot publicly provide
-# to run, uncomment lines 7-9 and 32-49 and comment out lines 27, 28, and 30
+  dplyr::rename(cell = dummyCell) %>% 
+  
+  
+  # the below is the script run, but is dependent on locational information, which we cannot publicly provide
+  # to run, uncomment lines  34-48 and comment out lines 27 -30
+  
   # dplyr::rename(x = X_COORDS,
   #               y= Y_COORDS) %>% 
-  # dplyr::select(year, SITE_NO, nHouse, x, y) %>%
+  # dplyr::select(year, SITE_NO, nHouse, CENTER, x, y) %>% 
   # sf::st_as_sf(# can turn into spatvector
   #   coords = c("x", "y"),
   #   crs = "EPSG:26912") %>%
-  # # First, calculate the cell that each site is within
-  # dplyr::mutate(
-  #   cell = 
-  #     terra::cellFromXY(
-  #       object = empty_rast, 
-  #       xy = sf::st_coordinates(geometry)
-  #     ) %>%
-  #     as.integer()
-  # ) %>% 
-  # sf::st_drop_geometry() %>%
+# # First, calculate the cell that each site is within
+# dplyr::mutate(
+#   cell = 
+#     terra::cellFromXY(
+#       object = empty_rast, 
+#       xy = sf::st_coordinates(geometry)
+#     ) %>%
+#     as.integer()
+# ) %>% 
+# sf::st_drop_geometry() %>%
 
-cell_sites <- reese_cells %>% 
-  # Aggregate nHouses by cell and year to get total houses per cell per year
-  dplyr::group_by(cell, year) %>%
+
+# Aggregate nHouses by cell and year to get total houses per cell per year
+dplyr::group_by(cell, year) %>%
   dplyr::summarise(nHouse = sum(nHouse, na.rm = FALSE)) %>%
   # Regroup by cells, and create nested tibbles
   dplyr::group_by(cell) %>%
   tidyr::nest(.key = "occupation") %>%
-  # Get the centerpoints of occupied cells, and turn back into an SF object
+  # Get the centerpoints of occupied cells, and turn back into an SF object. THESE WILL BE FALSE LOCATIONS
   dplyr::mutate(xy = terra::xyFromCell(handplant, cell) %>%
                   as.data.frame()) %>%
   tidyr::unnest(xy) %>% 
@@ -62,13 +64,13 @@ cell_sites <- reese_cells %>%
                crs = "EPSG:26912") 
 
 
-# We will focus below on the raster centers, so we can go from 200 to 4000 to capture all scales.
+# We will focus below on the raster centers, so we can go from 200 to 3400 to capture all scales.
 radii <-  
   seq(200, 4000, 200) %>%
   magrittr::add(1)# Add 1 to make sure the radii include cell centers (but just)
 
 # make a table the combines all radii options with their cells
-cell_variedRadii <- cell_sites %>%
+cells_variedRadii <- cells_sites %>%
   # Repeat all for each radius
   dplyr::cross_join(tibble::tibble(radius = radii)) %>%
   # group by radii, and get buffers for each group 
@@ -78,62 +80,58 @@ cell_variedRadii <- cell_sites %>%
   tibble::rownames_to_column(var = "ID") %>% 
   dplyr::mutate(ID = as.double(ID)) 
 
-
 #identify all fields within each radius option (this script may take awhile to run)
-cell_catchments <- cell_variedRadii %>% 
-  terra::extract(x = handplant, 
+cells_catchments <- cells_variedRadii %>% 
+  terra::extract(x = handplant, #> 0,
                  y = .,
                  cells = TRUE) %>%
   dplyr::rename(field = cell) %>% 
   dplyr::group_by(ID) %>%
   dplyr::nest_by(.key = "catchment") %>% 
   dplyr::left_join(., 
-                   cell_variedRadii %>% 
+                   cells_variedRadii %>% 
                      dplyr::select(ID, cell, radius), 
                    by = c("ID" = "ID")) %>% 
   dplyr::ungroup() %>% 
   sf::st_drop_geometry() %>% 
   dplyr::select(cell, radius, catchment)
 
-#export it to skip above steps in the future if necessary
-cell_catchments %>% saveRDS(here::here("data/data-derived/cells/cell_catchments.rds"))
-
+#export it to skip above step in the future if necessary
+cells_catchments %>% saveRDS(here::here("data/data-derived/cells/cells_catchments.rds"))
 
 ## past the long bit
-
-cell_catchments <- readRDS(here::here("data/data-derived/cells/cell_catchments.rds"))
+cells_catchments <- readRDS(here::here("data/data-derived/cells/cells_catchments.rds"))
 
 #make the series of weighted catchments according to the ratio described in the SI
 makeWeightedCatchments <- function(data, ratio){
-    purrr::map(
-      .x = ratio,
-      .f = function(thisratio){
-        
-        data %>% 
-          dplyr::filter(nFieldsNeeded < floor(nCell/thisratio)) %>% 
-          dplyr::group_by(cell, year) %>% 
-          dplyr::filter(nCell == min(nCell))
-      })  
+  purrr::map(
+    .x = ratio,
+    .f = function(thisratio){
+      
+      data %>% 
+        dplyr::filter(nFieldsNeeded < floor(nCell/thisratio)) %>% 
+        dplyr::group_by(cell, year) %>% 
+        dplyr::filter(nCell == min(nCell))
+    })  
 }
-
-# produce a datafram with each cell, year, nHouse, all radii, number of cells in catchment, list of catchment, and nFieldsNeeded
-cell_catchment_interim <- 
+# produce a dataframe with each cell, year, nHouse, all radii, number of cells in catchment, list of catchment, and nFieldsNeeded
+cells_catchment_interim <- 
   #produce each cell's yearly occupation list with each radius
-  cell_variedRadii %>% 
+  cells_variedRadii %>% 
   sf::st_drop_geometry() %>% 
   dplyr::select(-ID) %>% 
   #join catchment information
   dplyr::left_join(., 
                    dplyr::left_join(
                      # determine number of handplantable cells in each catchment 
-                     cell_catchments %>% 
+                     cells_catchments %>% 
                        tidyr::unnest(catchment) %>% 
                        dplyr::filter(handplant > 0 ) %>% 
                        dplyr::group_by(cell, radius) %>% 
                        dplyr::summarise(nCell = dplyr::n()) %>% 
                        dplyr::ungroup(), 
                      #keep the non-handplantable, will be used to determine catchment quality later
-                     cell_catchments %>% 
+                     cells_catchments %>% 
                        tidyr::unnest(catchment) %>% 
                        dplyr::select(-handplant) %>% 
                        dplyr::group_by(cell, radius) %>% 
@@ -142,50 +140,50 @@ cell_catchment_interim <-
                    by = c("cell" = "cell", "radius" = "radius")) %>% 
   #remove the extra 1 in the radius list
   dplyr::mutate(radius = radius - 1) %>% 
-  #unnest the occupation list so that it's more workable
+  #unnest the occupation list to work with it
   tidyr::unnest(occupation) %>%
   #determine number of 
   dplyr::mutate(nFieldsNeeded = nHouse) #this can be changed to alter the number of fields needed according to number of households
 
-cell_999 <- cell_catchment_interim %>% 
-  #only years AD 1000-1300 have this higher ratio
+#all years have this basic ratio
+cells_1_4 <-   cells_catchment_interim %>% 
+  makeWeightedCatchments(ratio = c(1,2,3,4 ))
+
+#only years AD 1000-1300 have these higher ratios
+cells_5_6 <- cells_catchment_interim %>% 
   dplyr::filter(year >999) %>% 
   makeWeightedCatchments(ratio = c(5,6))
 
-cell_fieldhouse <-  cell_catchment_interim %>% 
-  #only years AD 1000-1300 have this higher ratio. 
-  # Note that this extremely high ratio is excluded as we don't consider this ratio as part of the local catchment quality variable
+cells_fieldhouse <-  cells_catchment_interim %>% 
   dplyr::filter(year >999) %>% 
   makeWeightedCatchments(ratio = c(13))
 
-catchmentAll <-
-  cell_catchment_interim %>% 
-  #all years include ratios 1-4
-  makeWeightedCatchments(ratio = c(1,2,3,4 )) %>% 
+#combine and unnest catchment
+cells_catchmentAll <-
+  cells_1_4 %>% 
   #bind the other two groups and unnest
-  dplyr::bind_rows(., cell_999 ) %>% 
-  dplyr::bind_rows(., cell_fieldhouse ) %>% 
+  dplyr::bind_rows(., cells_5_6 ) %>% 
+  dplyr::bind_rows(., cells_fieldhouse ) %>% 
   dplyr::ungroup() %>%
   tidyr::unnest(catchment) 
 
 #calculate two catchment sizes
-maxCatch <- 
-  catchmentAll  %>% 
+maxCatch <- #the largest catchment ever considered
+  cells_catchmentAll  %>% 
   dplyr::group_by(cell, year) %>% 
   dplyr::summarize(maxRadius = max(radius))
 
-prefCatch <-   
-  cell_catchment_interim %>% 
-  makeWeightedCatchments(ratio = c(1,2,3,4 )) %>% 
-  dplyr::bind_rows(., cell_999 ) %>% 
+prefCatch <-   # the preferred catchment
+  cells_1_4  %>% 
+  dplyr::bind_rows(., cells_5_6 ) %>% 
   dplyr::ungroup() %>%
   tidyr::unnest(catchment) %>% 
   dplyr::group_by(cell, year) %>% 
   dplyr::summarize(prefRadius = max(radius))
 
 # combine all
-catchmentMaize_alldata <-
-  catchmentAll %>% 
+catchmentMaize_all <-
+  cells_catchmentAll %>% 
   dplyr::select(cell, year, nHouse, nFieldsNeeded, catchment) %>% 
   dplyr::group_by(cell, year, nHouse, nFieldsNeeded) %>% 
   dplyr::summarise(
@@ -194,26 +192,30 @@ catchmentMaize_alldata <-
         catchment %>%
           c())) %>% 
   dplyr::ungroup() %>% 
-  dplyr::left_join(y = maxCatch, by = c("cell" = "cell", "year" = "year"))%>% 
-  dplyr::left_join(y = prefCatch, by = c("cell" = "cell", "year" = "year"))
+  dplyr::left_join(y = maxCatch, by = c("cell" = "cell", "year" = "year")) %>% 
+  dplyr::left_join(y = prefCatch, by = c("cell" = "cell", "year" = "year")) %>% 
+  #join to get center status
+  dplyr::left_join(y = reese_cells %>% 
+                     dplyr::select(cell, year, CENTER), 
+                   by = c("cell" = "cell", "year" = "year"))%>% 
+  dplyr::rename(center = CENTER)
+
+#we'll use this for after the simulation
+catchmentMaize_all %>% 
+  saveRDS(here::here("data/data-derived/Simulated_catchment/catchmentMaize_all.RDS"))
 
 #we'll use this for the simulation
-catchmentMaize_alldata %>% 
+catchmentMaize_all %>% 
   dplyr::select(cell, year, nFieldsNeeded, catchment) %>% 
   saveRDS(here::here("data/data-derived/Simulated_catchment/catchmentMaize.RDS"))
 
+
 #just close to calculate local catchment quality
 catchmentClose <- 
-  cell_catchment_interim %>% 
-  makeWeightedCatchments(ratio = c(1,2,3,4 )) %>% 
-  dplyr::bind_rows(., cell_999 ) %>% 
-  # dplyr::bind_rows(., reese_fieldhouse ) %>% 
+  dplyr::bind_rows(cells_1_4 , cells_5_6 ) %>% 
+  # dplyr::bind_rows(., cells_fieldhouse ) %>% 
   dplyr::ungroup() %>%
-  tidyr::unnest(catchment) 
-
-  
-catchmentClose_alldata <- 
-  catchmentClose %>% 
+  tidyr::unnest(catchment)  %>% 
   dplyr::select(cell, year, nHouse, nFieldsNeeded, catchment) %>% 
   dplyr::group_by(cell, year, nHouse, nFieldsNeeded) %>% 
   dplyr::summarise(
@@ -225,7 +227,7 @@ catchmentClose_alldata <-
   dplyr::left_join(y = prefCatch, by = c("cell" = "cell", "year" = "year"))
 
 #we'll use this to calculate local catchment quality
-catchmentClose_alldata %>% 
+catchmentClose %>% 
   saveRDS(here::here("data/data-derived/Simulated_catchment/catchmentClose_0s.RDS"))
 
 
---
+++ R/7_createReeseBouts.R
@@ -0,0 +1,102 @@
+library(magrittr)
+library(terra)
+library(ggplot2)
+library(robinson2020)
+
+
+#assign the number of years (since 600) each cell has been occupied
+PriorOccupation <- function(data){
+  occupation <- 
+    purrr::map(.x = 1:nrow(data),
+               .f = function(i){
+                 data$occupation[[i]] %>% 
+                 tibble::rownames_to_column(var = "yearsPrior") 
+               }) 
+    
+   occupation %>% return()
+}
+
+
+# calculate start and end dates for each occupation bouts
+bouts_broad <-
+  #read in catchment 
+  readRDS(here::here("data/data-derived/Simulated_catchment/catchmentMaize_all.RDS")) %>% 
+  #rename to remind the reviewers that these are not real locations
+  dplyr::rename(dummyCell = cell) %>% 
+  #isolate to just the relevant data
+  dplyr::select(dummyCell, year, nHouse) %>% 
+  tibble::as_tibble() %>%
+  dplyr::group_by(dummyCell) %>% 
+  # identify each occupation bout start and end time. This is clunky but it works
+  dplyr::mutate(start = ifelse(year %>% diff() %>% c(0 , .) == 1, NA, year),
+                end = ifelse(year %>% diff() %>% c(. , 0) == 1,  NA, year)) %>% 
+  dplyr::filter(!is.na(start) | !is.na(end) ) %>% 
+  dplyr::select(dummyCell, start, end )  %>% 
+  dplyr::group_by(dummyCell) %>% 
+  dplyr::reframe(start = as.numeric(na.omit(start)),
+                 end = as.numeric(na.omit(end))) %>% 
+  dplyr::group_by(dummyCell) %>% 
+  dplyr::mutate(boutLength = end - start) %>% 
+  dplyr::filter(boutLength > 1) %>% 
+  #number the bouts sequentially as a useful way to differentiate the bouts
+  dplyr::mutate(boutNo = seq(1:dplyr::n())) 
+
+# removing incredibly long occs
+bouts <- 
+  bouts_broad %>% 
+  dplyr::ungroup() %>% 
+  #assign VEP period
+  dplyr::left_join(robinson2020::vep_demography %>% 
+                     dplyr::filter(`Study Area` == "CMV") %>% 
+                     dplyr::select(Period, Start, End) %>% 
+                     dplyr::rowwise() %>%
+                     dplyr::mutate(year = list(Start:(End - 1))) %>%
+                     tidyr::unnest(year) %>%
+                     dplyr::rename(vep = Period) %>% 
+                     dplyr::select(vep, year),
+                   by = c("end" = "year")) %>% 
+  #only keep VEP periods 6-19
+  dplyr::filter(vep < 20) %>% 
+  dplyr::rowwise() %>% 
+  #make long
+  dplyr::mutate(year = list(start:end)) %>% 
+  #remove start and end dates
+  dplyr::select(dummyCell, year, boutLength, boutNo, vep) %>% 
+  tidyr::unnest(year) %>% 
+  #join the annual number of contemporaneous houses
+  dplyr::left_join(., 
+                   readRDS(here::here("data/data-derived/Simulated_catchment/catchmentMaize_all.RDS")) %>% 
+                     #rename to remind the reviewers that these are not real locations
+                     dplyr::rename(dummyCell = cell) %>% 
+                     #isolate to just the relevant data
+                     dplyr::select(dummyCell, year, nHouse, center),
+                   by = c("dummyCell" = "dummyCell", "year" = "year") ) %>% 
+  dplyr::group_by(dummyCell, boutLength, boutNo, vep) %>% 
+  #calculate the maximum number of houses per dummyCell
+  dplyr::summarise(maxHouse = max(nHouse),
+                   center = max(center)) %>% 
+  #remove very long occupation lengths
+  dplyr::filter(maxHouse * 80 > boutLength) %>%  # assume max boutLength is double that of Varien 2007 (40 years doubled to 80 years ) and ensure there are enough houses (80 years each) to cover the boutLength 
+  dplyr::ungroup() %>% 
+  dplyr::select(-maxHouse) %>%
+  # bring back the start and end dates
+  dplyr::left_join(bouts_broad,
+                   by = c("dummyCell"= "dummyCell", "boutLength" = "boutLength", "boutNo" = "boutNo")) %>% 
+  dplyr::group_by(dummyCell) %>% 
+  dplyr::arrange(start) %>% 
+  dplyr::mutate(boutNo = seq(1:dplyr::n())) #renumber boutNos
+
+
+#export and save
+bouts %>% saveRDS(here::here("data/data-derived/prepAnalysis/bouts.rds"))
+
+bouts <- readRDS(here::here("data/data-derived/prepAnalysis/bouts.rds"))
+
+bouts %>% 
+  dplyr::rowwise() %>% 
+  dplyr::mutate(year = list(start:end)) %>% 
+  dplyr::select(-start, -end) %>% 
+  tidyr::unnest(year) %>% 
+  saveRDS(here::here("data/data-derived/prepAnalysis/yearlybouts.rds"))
+
+
